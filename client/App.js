9c90e3a7f463ca45d9237f0d5a0067b4dcb7338d
Jzz-C <ygyi8632@gmail.com>
2023-08-03 17:32:19 +0630

    feat: test react navigation and download other packages

--- client/App.js
+++ client/App.js
@@ -1,13 +1,37 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+// In App.js in a new project
 
-export default function App() {
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+function HomeScreen() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-red-700 text-lg font-semibold">
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center">
+      <Text>Home Screen</Text>
     </View>
   );
 }
+
+function Restaurant() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Restaurant Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Restaurant" component={Restaurant} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
