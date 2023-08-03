5f50e43ff4daeb05aa33819728a61fdd2038b56c
Jzz-C <ygyi8632@gmail.com>
2023-08-03 23:09:24 +0630

    feat: added search bar ui and test use

--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -1,11 +1,56 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView, StatusBar, TextInput } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import * as Icon from "react-native-feather";
+
+import { themeColors } from "../theme";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  const [activeColor, setActiveColor] = React.useState(false);
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({ headerShown: false });
+  }, []);
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView className="bg-white">
+      <StatusBar barStyle={"dark-content"} backgroundColor={"#fff"} />
+      <View className="mx-4 mt-2 space-y-3">
+        {/* Search Bar */}
+        <View className="flex-row items-center space-x-2">
+          <View
+            style={{ borderColor: themeColors.text }}
+            className="flex-row flex-1 items-center p-2 border-[1.5px] rounded-xl"
+          >
+            <Icon.Search width={20} height={20} stroke={themeColors.text} />
+            <TextInput placeholder="Restaurant" className="ml-2 flex-1" />
+            <View
+              style={{ borderColor: themeColors.text }}
+              className="flex-row items-center space-x-1 border-l-2 pl-1"
+            >
+              <Icon.MapPin width={18} height={18} stroke={themeColors.text} />
+              <Text className="text-gray-500">New York, NYC</Text>
+            </View>
+          </View>
+          <View
+            style={{
+              borderColor: themeColors.text,
+              backgroundColor: activeColor ? themeColors.bgColor(2) : "white",
+            }}
+            className="p-3 rounded-2xl border-[1.5px]"
+          >
+            <Icon.Sliders
+              width={23}
+              height={23}
+              stroke={activeColor ? "gray" : themeColors.text}
+              onPress={() => setActiveColor(!activeColor)}
+            />
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
