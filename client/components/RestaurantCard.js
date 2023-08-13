6fdb013c00a691f517213639e39e50bc0819db47
Jzz-C <ygyi8632@gmail.com>
2023-08-13 22:02:22 +0630

    feat: added restaurant route and ui

--- client/components/RestaurantCard.js
+++ client/components/RestaurantCard.js
@@ -1,10 +1,19 @@
 import React from "react";
-import { View, Text, ScrollView, Image } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableWithoutFeedback,
+} from "react-native";
 import * as Icon from "react-native-feather";
+import { useNavigation } from "@react-navigation/native";
 
 import { themeColors } from "../theme";
 
 const RestaurantCard = ({ restaurants }) => {
+  const navigation = useNavigation();
+
   return (
     <ScrollView
       className="overflow-visible px-2 py-1"
@@ -14,42 +23,35 @@ const RestaurantCard = ({ restaurants }) => {
         padding: 8,
       }}
     >
-      {restaurants.map(
-        ({
-          id,
-          name,
-          image,
-          address,
-          stars,
-          reviews,
-          category,
-          description,
-        }) => (
+      {restaurants.map((item) => (
+        <TouchableWithoutFeedback
+          key={item.id}
+          onPress={() => navigation.navigate("Restaurant", { ...item })}
+        >
           <View
-            key={id}
             className="rounded-xl shadow-2xl mr-8 bg-white"
             style={{
               shadowColor: themeColors.bgColor(5),
             }}
           >
-            <Image source={image} className="h-36 w-60 rounded-t-xl" />
+            <Image source={item.image} className="h-36 w-60 rounded-t-xl" />
 
             <View className="px-3 pb-4 space-y-2">
-              <Text className="font-bold pt-2">{name}</Text>
+              <Text className="font-bold pt-2">{item.name}</Text>
               <View className="flex-row items-center space-x-1">
                 <Image
                   source={require("../assets/images/fullStar.png")}
                   className="h-4 w-4"
                 />
                 <Text className="text-xs">
-                  <Text className="text-green-700">{stars}</Text>
+                  <Text className="text-green-700">{item.stars}</Text>
                   <Text className="text-xs text-gray-500">
                     {" "}
-                    ({reviews} review)
+                    ({item.reviews} review)
                   </Text>{" "}
                   ·{" "}
                   <Text className="font-semibold text-xs text-gray-500">
-                    {category}
+                    {item.category}
                   </Text>
                 </Text>
               </View>
@@ -57,13 +59,13 @@ const RestaurantCard = ({ restaurants }) => {
                 <Icon.MapPin color="gray" width={15} height={15} />
                 <Text className="text-xs text-gray-500">
                   {" "}
-                  Nearby · {address}
+                  Nearby · {item.address}
                 </Text>
               </View>
             </View>
           </View>
-        )
-      )}
+        </TouchableWithoutFeedback>
+      ))}
     </ScrollView>
   );
 };
--- client/screens/RestaurantScreen.js
+++ client/screens/RestaurantScreen.js
@@ -1,18 +1,44 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StatusBar,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import * as Icon from "react-native-feather";
+import { themeColors } from "../theme";
 
 const RestaurantScreen = () => {
   const navigation = useNavigation();
+  const { params: item } = useRoute();
 
   React.useLayoutEffect(() => {
     navigation.setOptions({ headerShown: false });
   }, []);
 
   return (
-    <View>
-      <Text>RestaurantScreen</Text>
-    </View>
+    <SafeAreaView>
+      {/* <StatusBar barStyle={"light-content"} backgroundColor={"black"} /> */}
+      <View className="relative">
+        <Image source={item.image} className="w-full h-3/4" />
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={{ backgroundColor: themeColors.bgColor(2) }}
+          className="absolute p-2 rounded-full shadow top-2 left-2"
+        >
+          <Icon.ArrowLeft strokeWidth={4} stroke={"white"} />
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{ borderTopLeftRadius: 45, borderTopRightRadius: 45 }}
+        className="-mt-12 pt-6 bg-white"
+      >
+        <Text>Hello</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
