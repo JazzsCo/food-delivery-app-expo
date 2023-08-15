e4868da3edbaaef75f133e7b586201584de289e9
Jzz-C <ygyi8632@gmail.com>
2023-08-15 22:28:28 +0630

    feat: design menus rows and added

--- client/screens/RestaurantScreen.js
+++ client/screens/RestaurantScreen.js
@@ -3,14 +3,16 @@ import {
   View,
   Text,
   SafeAreaView,
-  StatusBar,
   Image,
   TouchableOpacity,
   ScrollView,
+  ImageBackground,
 } from "react-native";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import * as Icon from "react-native-feather";
+
 import { themeColors } from "../theme";
+import Menus from "../components/Menus";
 
 const RestaurantScreen = () => {
   const navigation = useNavigation();
@@ -23,7 +25,7 @@ const RestaurantScreen = () => {
   return (
     <SafeAreaView>
       <View className="relative">
-        <Image source={item.image} className="w-full h-[75%]" />
+        <ImageBackground source={item.image} className="w-full h-[75%]" />
         <TouchableOpacity
           onPress={() => navigation.goBack()}
           style={{ backgroundColor: themeColors.bgColor(2) }}
@@ -31,45 +33,35 @@ const RestaurantScreen = () => {
         >
           <Icon.ArrowLeft strokeWidth={4} stroke={"white"} />
         </TouchableOpacity>
-      </View>
-      <View className="-mt-96 pt-6 px-3 bg-white rounded-t-3xl">
-        <Text className="text-2xl font-semibold">{item.name}</Text>
-        <View className="mt-2 flex-row items-center space-x-1">
-          <Image
-            source={require("../assets/images/fullStar.png")}
-            className="h-4 w-4"
-          />
-          <Text className="text-base text-green-700">{item.stars}</Text>
-          <Text className="text-base text-gray-700">
-            {" "}
-            {item.reviews} review
-          </Text>
-          <View className="flex-row items-center">
-            <Icon.MapPin color="black" width={15} height={15} />
+        <View className="absolute top-2/4 w-full pt-6 px-3 bg-white rounded-t-3xl">
+          <Text className="text-2xl font-semibold">{item.name}</Text>
+          <View className="mt-2 flex-row items-center space-x-1">
+            <Image
+              source={require("../assets/images/fullStar.png")}
+              className="h-4 w-4"
+            />
+            <Text className="text-base text-green-700">{item.stars}</Text>
             <Text className="text-base text-gray-700">
               {" "}
-              Nearby · {item.address}
+              {item.reviews} review
             </Text>
-          </View>
-        </View>
-        <Text className="text-2xl font-semibold mt-3">Menus</Text>
-        <ScrollView showsVerticalScrollIndicator={false} className="mt-3">
-          {item.dishes.map(({ id, name, description, price, image }) => (
-            <View
-              key={id}
-              className="flex-row w-full p-3 rounded-xl shadow-3xl mb-3 bg-gray-200"
-            >
-              <Image source={image} className="w-24 h-24 rounded-xl shadow" />
-              <View className="ml-3 flex-1 space-y-1">
-                <Text className="text-xl font-semibold">{name}</Text>
-                <Text className="text-base text-gray-700">{description}</Text>
-                <View className="flex-row justify-between">
-                  <Text className="text-lg font-medium">$ {price}</Text>
-                </View>
-              </View>
+            <View className="flex-row items-center">
+              <Icon.MapPin color="black" width={15} height={15} />
+              <Text className="text-base text-gray-700">
+                {" "}
+                Nearby · {item.address}
+              </Text>
             </View>
-          ))}
-        </ScrollView>
+          </View>
+          <Text className="text-2xl font-semibold mt-3">Menus</Text>
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{ paddingBottom: 200 }}
+            className="mt-3"
+          >
+            <Menus {...item} />
+          </ScrollView>
+        </View>
       </View>
     </SafeAreaView>
   );
