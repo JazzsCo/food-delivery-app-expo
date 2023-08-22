76e3614d90a1ad5569ff9fd9a1fc933929d2f19e
Jzz-C <ygyi8632@gmail.com>
2023-08-22 16:50:04 +0630

    feat: design delivery screen

--- client/screens/DeliveryScreen.js
+++ client/screens/DeliveryScreen.js
@@ -1,12 +1,16 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import * as Icon from "react-native-feather";
 import MapView, { Marker } from "react-native-maps";
 
 import { featured } from "../constants";
 import { themeColors } from "../theme";
+import { useNavigation } from "@react-navigation/native";
 
 const DeliveryScreen = () => {
   const item = featured.restaurants[0];
+
+  const navigation = useNavigation();
 
   return (
     <View className="flex-1">
@@ -30,6 +34,63 @@ const DeliveryScreen = () => {
           pinColor={themeColors.bgColor(3)}
         />
       </MapView>
+
+      <View className="rounded-t-3xl -mt-12 p-5 bg-white border-t-gray-900">
+        <View className="flex-row justify-between pt-4 px-2">
+          <View>
+            <Text className="text-lg text-gray-700 font-semibold">
+              Estimated Arrival
+            </Text>
+            <Text className="text-3xl font-extrabold text-gray-700">
+              20-30 Minutes
+            </Text>
+            <Text className="mt-2 text-gray-700 font-semibold">
+              Your Order is own its way
+            </Text>
+          </View>
+          <Image
+            className="h-24 w-24"
+            source={require("../assets/images/bikeGuy2.gif")}
+          />
+        </View>
+
+        <View
+          style={{ backgroundColor: themeColors.bgColor(0.8) }}
+          className="p-2 flex-row justify-between items-center rounded-full mt-3 -mb-2"
+        >
+          <View
+            style={{ backgroundColor: "rgba(255,255,255,0.4)" }}
+            className="p-1 rounded-full"
+          >
+            <Image
+              style={{ backgroundColor: "rgba(255,255,255,0.4)" }}
+              className="w-16 h-16 rounded-full"
+              source={require("../assets/images/deliveryGuy.png")}
+            />
+          </View>
+
+          <View className="flex-1 ml-3">
+            <Text className="text-lg font-bold text-white">Syed Noman</Text>
+            <Text className="text-white font-semibold">Your Rider</Text>
+          </View>
+          <View className="flex-row items-center space-x-3 mr-3">
+            <TouchableOpacity className="bg-white p-2 rounded-full">
+              <Icon.Phone
+                fill={themeColors.bgColor(1)}
+                stroke={themeColors.bgColor(1)}
+                strokeWidth="1"
+              />
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Home")}
+              className="bg-white p-2 rounded-full"
+            >
+              <Icon.X stroke={"red"} strokeWidth="5" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
